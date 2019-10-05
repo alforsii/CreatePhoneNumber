5fdeaefe5da60c14a18e762196c972dc3dde8d51
Ashrafzhon Kurbonaliev <alforsii@gmail.com>
2019-10-05 02:13:10 -0400

    First commit - This is my module0 assignment

---
+++ createPhoneNumber.js
@@ -0,0 +1,13 @@
+
+
+function createPhoneNumber(numbers){
+    let format = "(xxx) xxx-xxxx";
+    
+    for(let i = 0; i < numbers.length; i++){
+      format = format.replace('x', numbers[i]);
+    }
+    
+    return format;
+  }
+  let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9,0];
+  createPhoneNumber(arr);
